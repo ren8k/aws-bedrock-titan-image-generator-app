dafbb39b66940cb2350927d2e867e1c9b0ec2592
renya <rsmilek5492@gmail.com>
2025-01-05 20:42:29 +0900

    update AWS region and model ID in config

--- src/app/config.py
+++ src/app/config.py
@@ -1,3 +1,3 @@
-AWS_REGION = "us-west-2"
-MODEL_ID = "amazon.titan-image-generator-v2:0"
+AWS_REGION = "us-east-1"  # "us-west-2"
+MODEL_ID = "amazon.nova-canvas-v1:0"  # "amazon.titan-image-generator-v2:0"
 SAVE_DIR = "../../images/output"
